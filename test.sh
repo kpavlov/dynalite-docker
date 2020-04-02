50f3051260a9b0fddf279176cebfe2aedcab87a9
Konstantin Pavlov <kpavlov@users.noreply.github.com>
2020-04-02 13:02:23 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
-docker build -t kpavlov/dynamodb-local:latest .
+docker build --force-rm --no-cache -t kpavlov/dynamodb-local:dev .
 docker-compose up --remove-orphans --renew-anon-volumes
 docker-compose rm --force -v
